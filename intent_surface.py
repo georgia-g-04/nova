47ada535a18d6c76e1887bca621b16e84f037749
Georgia Gill <u7248014@anu.edu.au>
2026-07-06 22:44:13 +1000

    save minor updates

--- intent_surface.py
+++ intent_surface.py
@@ -10,10 +10,7 @@ import time
 
 load_dotenv()
 
-
 timestamp = time.localtime()
-decision_log = pd.DataFrame(columns=['Timestamp', 'Immediate context', 'Importance of immediate context', 'Behvaioural context', 'Importance of behavioural context', 'Physiological context', 'Importance of physiological context', 'User intent', 'Final decision', 'Future impacts'])
-print(decision_log)
 
 def generate(user_input):
     client = genai.Client(
@@ -38,14 +35,15 @@ def generate(user_input):
                                  
 f"""You are the AI assistant for a new novel personal computing device that manages attention, autonomy and privacy. Please write with Australian English spelling. Assume the timezone is Sydney Australian time. 
 
-The user will ask for questions and information relating to oral functions on a mobile phone. Your job is to:
-- Determine the user’s intent. This is done through layers of information as seen below. 
-1. Immediate context. What app are they asking for? What task are they asking for? What time of day are they asking and where are they asking from?
-2. Behavioural information. (the users routines, past information and previous corrections)
-3. physiological signals (take inputs from electronic components If available that provide information about the user's posture, gaze and voice tone)
+The user will ask for questions and information relating to oral functions on a mobile phone. Your job is to determine the user’s intent. This is done through layers of information as seen below. 
+    1. Immediate context. What app are they asking for? What task are they asking for? What time of day are they asking and where are they asking from?
+    2. Behavioural information. (the users routines, past information and previous corrections)
+    3. physiological signals (take inputs from electronic components If available that provide information about the user's posture, gaze and voice tone)
 
-If input = /current_status: your job is to return the following outputs. The idea is you should be able to infer user intent based off a mixture of immediate context, behavioural context and physiological context without the user having to prompt anything. 
-/current_status is a system prompt - for example, imagine you are schedule to run /current_status every 10 minutes to update the UI. 
+If input = /current_status: 
+Your job is to return the following outputs. 
+Why? The idea is you should be able to infer user intent based off a mixture of immediate context, behavioural context and physiological context without the user having to prompt anything. 
+/current_status is a system prompt - for example, imagine you are scheduled to run /current_status every 10 minutes to update the UI. 
 
 - Determine if we have enough information to provide a response.
     - If yes, provide a description of the context gathered, and generate a brief JSON description of the response.
