61fd11e07816f2933b49234b7192fa0c26563c2c
Georgia Gill <u7248014@anu.edu.au>
2026-07-25 22:41:19 +1000

    still working on state estimator, am working on adding in confidence with logprobs but need to figure out the metadata

--- nova_v1/backend/app/state_estimator/state_estimator.py
+++ nova_v1/backend/app/state_estimator/state_estimator.py
@@ -10,6 +10,8 @@ WHO USES THIS
 
 """
 
+# debug python -m state_estimator.state_estimator
+
 # import necessary libraries and set up environment
 import os
 from openai import OpenAI
@@ -31,7 +33,7 @@ model = 'gpt-5.6-luna'
 system_prompt = f"""{default_system_prompt}
 
 You will receive input in the format of {UserStateInput}. Your job is to look at the current user state and 
-make provide a one sentence summary of the current user state. 
+provide a one sentence summary of the current user state. 
 """ # debug, need to do more research how to 'classify' user state
 
 # call ai client
@@ -39,17 +41,33 @@ def generate_response(user_input):
     response = client.responses.parse(
         model=model,
         instructions = system_prompt,
-        text_format = UserStateOutput,
+        #text_format = UserStateOutput,
         input = user_input
     )
     print(response)
+    response.metadata['log']
     return response
 
-# test user_input_state
+def calculate_confidence_linear_probs(log_probs): # debug from https://gautam75.medium.com/unlocking-llm-confidence-through-logprobs-54b26ed1b48a accessed 25/07/2026
+    linear_probs = np.round(np.exp(log_probs)*100,2)
+    confidence = np.mean(linear_probs)
+    return confidence  # closer to 100 indicates higher confidence
 
+def calculate_log_probs(response):
+    joint_logprob=0.0
+
+    count=0
+    for prob in response.metadata['logprobs']['content']:
+        
+        count+=1
+        joint_logprob += np.round(np.exp(prob['logprob']) * 100,2)
+
+    print("Joint prob:", np.round(joint_logprob/count , 2), "%")
 
 if __name__ == '__main__': # debug, integrate
     print("Hello!")
-    user_input = input()
-    generate_response(user_input)
+    while True:
+        user_input = input()
+        response = generate_response(user_input)
+        calculate_log_probs(response)
     
